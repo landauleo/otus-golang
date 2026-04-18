e99fc6a76c5ad7e3eca1251fb6f3a0263def3319
landauleo <bambie7@mail.ru>
2026-04-18 16:13:56 +0300

    hw04_lru_cache_part1

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,102 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	//изначально в структуре было только поле List, чтобы код скомпилировался
+	//так как если мы делаем такое встраивание или embedding -> означает, что эта структура реализует интерфейс List,
+	//даже если его методы пока не реализованы
+
+	//так как список двухсвязный, иным способом до данных не добраться
+	first *ListItem
+	last  *ListItem
+	size  int
+}
+
+func (l *list) Len() int {
+	return l.size
+}
+
+func (l *list) Front() *ListItem {
+	return l.first
+}
+
+func (l *list) Back() *ListItem {
+	return l.last
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := new(ListItem)
+	newItem.Value = v
+
+	if l.size == 0 {
+		l.first = newItem
+		l.last = newItem
+	} else {
+		newItem.Next = l.first
+		l.first.Prev = newItem
+		l.first = newItem
+	}
+	l.size++
+	return newItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := new(ListItem)
+	newItem.Value = v
+
+	if l.size == 0 {
+		l.first = newItem
+		l.last = newItem
+	} else {
+		newItem.Prev = l.last
+		l.last.Next = newItem
+		l.last = newItem
+	}
+	l.size++
+	return newItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == l.first {
+		l.first = i.Next
+	}
+	if i == l.last {
+		l.last = i.Prev
+	}
+
+	//"замыкаем" соседей убранного элемента
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	l.size--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	//первый элемент не проверяем, так как он уже в начале списка
+	if i == l.first {
+		return
+	}
+
+	if i == l.last {
+		l.last = i.Prev
+	}
+
+	//"замыкаем" соседей убранного элемента
+	i.Prev.Next = i.Next
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	i.Prev = nil
+	i.Next = l.first
+	l.first.Prev = i
+	l.first = i
 }
 
 func NewList() List {
-	return new(list)
+	return new(list) //тип указатель - *list
 }
