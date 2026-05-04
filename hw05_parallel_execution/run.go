af1c0c6967f25cc6437c4ce8b924dd057bf8ce9d
landauleo <bambie7@mail.ru>
2026-05-04 21:22:31 +0300

    hw05_parallel_execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -16,9 +16,25 @@ func Run(tasks []Task, n, m int) error {
 	var wg sync.WaitGroup
 	var errorCounter atomic.Int64
 
-	for i := 0; i < len(tasks); i++ {
-		if errorCounter.Load() <= int64(m) {
-			go worker(tasks[i], &wg, &errorCounter, m)
+	tasksChannel := make(chan Task)
+	stopChannel := make(chan struct{})
+	var once sync.Once
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go worker(&wg, &errorCounter, m, stopChannel, tasksChannel, &once)
+	}
+
+	shouldStop := false //иначе из внешнего цикла не выйти (на моем уровне знаний)
+	for _, t := range tasks {
+		select {
+		case <-stopChannel:
+			shouldStop = true
+		case tasksChannel <- t:
+		}
+
+		if shouldStop {
+			break
 		}
 	}
 
@@ -31,13 +47,28 @@ func Run(tasks []Task, n, m int) error {
 	return nil
 }
 
-func worker(task Task, wg *sync.WaitGroup, errorCounter *atomic.Int64, m int) {
-	//if errorCounter.Load() <= int64(m) {
-	wg.Add(1)
-	result := task()
-	if result != nil {
-		errorCounter.Add(1)
+// channel уже содержит указатели внутри себя на реальные данные в памяти -> передаем по значению
+// WaitGroup, Once - нет -> передаем по указателю
+func worker(wg *sync.WaitGroup, errorCounter *atomic.Int64, m int, stopChannel chan struct{}, tasksChannel chan Task, once *sync.Once) {
+	defer wg.Done()
+	for {
+		select {
+		case <-stopChannel:
+			return
+		case task, ok := <-tasksChannel: //ok значит только то, что в канале были данные и их вычитали
+			if !ok {
+				return
+			}
+
+			if err := task(); err != nil { //err - уже результат выполнения таски
+				newErrCounter := errorCounter.Add(1)
+				if newErrCounter >= int64(m) {
+					once.Do(func() {
+						close(stopChannel)
+					})
+					return
+				}
+			}
+		}
 	}
-	wg.Done()
-	//}
 }
