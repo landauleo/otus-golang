70921b10e43320820c613f37a8f382289a50f163
landauleo <bambie7@mail.ru>
2026-06-06 23:07:59 +0300

    hw08_envdir_tool

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,66 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		return 1 // Нет команды для запуска
+	}
+
+	//создаем команду
+	command := exec.Command(cmd[0], cmd[1:]...)
+
+	//если это не сделать, логи будут писаться в stdout самой программы, в терминале мы их не увидим
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	//нельзя не "копировать" остальное окружение, если брать только то, что пришло из аргумента функции,
+	//то у нас не будет ли текущей директории, ни кодировки и прочее
+	rawEnv := os.Environ()
+
+	tempMap := make(map[string]string)
+
+	for _, kv := range rawEnv {
+		k, v, _ := strings.Cut(kv, "=")
+		tempMap[k] = v
+	}
+
+	//добавляем/удаляем из tempMap переменные из аргументов
+	for k, v := range env {
+		if v.NeedRemove {
+			delete(tempMap, k)
+		} else {
+			tempMap[k] = v.Value
+		}
+	}
+
+	//обратно всё склеиваем
+	var finalEnv []string
+	for k, v := range tempMap {
+		finalEnv = append(finalEnv, k+"="+v)
+	}
+
+	//отдаем изолированное окружение команде
+	command.Env = finalEnv
+
+	//запуск
+	err := command.Run()
+	if err != nil {
+		var exitError *exec.ExitError
+		//ищем первую попавшуюся системную ошибку
+		if errors.As(err, &exitError) {
+			//код выхода утилиты должен совпадать с кодом выхода программы
+			return exitError.ExitCode()
+		}
+		return 1
+	}
+
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,29 @@
 package main
 
+import "os"
+
+// проверка:
+// ./go-envdir ./my_env_dir env
+// env просто печатает в консоль все переменные окружения, которые ей доступны)
+// в ./my_env_dir  разные файлики с корнер-кейсами из ТЗ
 func main() {
-	// Place your code here.
+	// os.Args[0] — имя программы
+	// os.Args[1] — путь к папке
+	// os.Args[2] — сама команда
+	if len(os.Args) < 3 {
+		os.Exit(1)
+	}
+
+	dir := os.Args[1]
+	commandWithArgs := os.Args[2:]
+
+	env, err := ReadDir(dir)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	exitCode := RunCmd(commandWithArgs, env)
+
+	// 3. Завершаем утилиту с тем же кодом
+	os.Exit(exitCode)
 }
