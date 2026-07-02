c679f0fc563a87873d61bc442bf5352867941bd6
landauleo <bambie7@mail.ru>
2026-07-03 01:10:03 +0300

    hw09_struct_validator

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,6 +2,7 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -11,13 +12,13 @@ type UserRole string
 // Test the function on different structures and other types.
 type (
 	User struct {
-		ID     string `json:"id" validate:"len:36"`
+		ID     string `json:"id" validate:"len:36"` //всё, что с обратными кавычками - Struct Tags
 		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		Age    int      `validate:"min:18|max:50"`
+		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role   UserRole `validate:"in:admin,stuff"`
+		Phones []string `validate:"len:11"`
+		meta   json.RawMessage
 	}
 
 	App struct {
@@ -36,25 +37,53 @@ type (
 	}
 )
 
-func TestValidate(t *testing.T) {
+func TestValidate(t *testing.T) { //главный тест-родитель (внешняя t) -> через это Go сообщает ОС, упал ли весь тест
 	tests := []struct {
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Name:   "Puk",
+				Age:    20,
+				Email:  "test@example.com",
+				Role:   "admin",
+				Phones: []string{"79991112233", "79992223344"},
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			in: App{
+				Version: "1",
+			},
+			expectedErr: errors.New("error"),
+		},
+		{
+			in: Response{
+				Code: 200,
+			},
+			expectedErr: nil,
+		},
+		{
+			in:          Token{Header: []byte("h"), Payload: []byte("p")},
+			expectedErr: nil,
+		},
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
+	for i, testCase := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) { //тест-ребенок (внутренняя t) -> изолированный пульт-управления
+			tc := testCase
 			t.Parallel()
+			err := Validate(tc.in)
 
-			// Place your code here.
-			_ = tt
+			if tc.expectedErr == nil && err != nil {
+				t.Errorf("expected no error, got: %v", err)
+			}
+
+			if tc.expectedErr != nil && err == nil {
+				t.Errorf("expected error, got nil")
+			}
 		})
 	}
 }
