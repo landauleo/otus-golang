eb280d5d307f8938d5d80a95dc383bcc496ddb10
landauleo <bambie7@mail.ru>
2026-08-11 00:55:33 +0300

    hw10_program_optimization

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -3,54 +3,32 @@ package hw10programoptimization
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"io"
 	"strings"
 )
 
 type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
+	Email string `json:"email"` //вот это имба
 }
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	emails, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(emails, domain)
+	return getEmails(r, domain)
 }
-
-type emails [100_000]string //это массив!!!
 
 // ВАЖНО: когда даешь имена возвращаемым значениям,
 // язык создает их в начале вызова функции и инициализирует нулевыми значениями
-func getUsers(r io.Reader) (result emails, err error) {
+func getEmails(r io.Reader, domain string) (DomainStat, error) {
+	result := make(DomainStat)
 	scanner := bufio.NewScanner(r)
-	var i = 0
 	var user User
 
 	for scanner.Scan() {
 		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
 			continue
 		}
-		result[i] = user.Email
-		i++
-	}
-	return
-}
-
-func countDomains(e emails, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, email := range e {
+		email := user.Email
 		//at - это то, как символ @ читается на ангельском
 		atIndex := strings.Index(email, "@")
 		if atIndex == -1 {
